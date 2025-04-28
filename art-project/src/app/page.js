d95b8710346137e5788966786a7942a90e32f612
Claire Draeger <claire.a.draeger.gr@dartmouth.edu>
2025-04-28 14:11:03 -0400

    fixing apple pencil offset?

--- art-project/src/app/page.js
+++ art-project/src/app/page.js
@@ -33,15 +33,16 @@ export default function Home() {
     const draw = (e) => {
       if (!isPainting) return;
     
-      ctx.lineWidth = lineWidthRef.current;
       ctx.strokeStyle = strokeStyleRef.current;
       ctx.lineCap = "round";
       ctx.lineJoin = "round";
     
-      const x = e.offsetX;
-      const y = e.offsetY;
+      const rect = canvas.getBoundingClientRect();
+      const x = (e.clientX - rect.left);
+      const y = (e.clientY - rect.top);
       const brush = brushTypeRef.current;
       const pressure = e.pressure || 0.5; // default pressure if not supported
+      ctx.lineWidth = lineWidthRef.current * pressure;
     
       if (brush === "round") {
         ctx.lineTo(x, y);
